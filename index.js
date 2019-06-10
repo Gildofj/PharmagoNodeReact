3aa64e4a4135b74bf779fa3a9c277d2f25857b3a
Gildo Júnior <1gildojunior@gmail.com>
2019-06-10 00:30:06 -0300

    Google oauth - funcional

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+//External imports
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+//Internal Imports
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'})
-});
+//https://console.developers.google.com
+ 
+//Passport autentication
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, () => {
+        console.log(acessToken);
+    })
+);
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+//server connect
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
