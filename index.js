2059342c390ce08d97d2c637d8f1d424639aa077
Gildo Júnior <1gildojunior@gmail.com>
2019-06-10 01:59:49 -0300

    Database Persist: on

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 //External imports
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+require('./services/passport');
 const keys = require('./config/keys');
+
+//Connect in Database
+mongoose.connect(keys.mongoURI);
+mongoose.connection.on('error', console.error.bind(console, 'Connection error!'));
+mongoose.connection.once('open', () => {
+    console.log('Connected to database!');
+});
 
 //Internal Imports
 const app = express();
 
-//https://console.developers.google.com
- 
-//Passport autentication
-passport.use(new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, () => {
-        console.log(acessToken);
-    })
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
+require('./routes/authRoutes')(app);
 
 //server connect
 const PORT = process.env.PORT || 3000;
