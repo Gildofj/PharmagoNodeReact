b8a659715239250cc8cc66a4dde45b60458eb78d
Gildo Júnior <1gildojunior@gmail.com>
2019-06-14 01:15:40 -0300

    correcao

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./dev');
-} else {
     module.exports = require('./prd');
+} else {
+    module.exports = require('./dev');
 }
