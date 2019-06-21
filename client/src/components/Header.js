c2e4f4ab2c9d9fbfd3c5ec578cbb601145e3debe
Gildo Júnior <1gildojunior@gmail.com>
2019-06-21 20:15:12 -0300

    Header start

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,21 @@ class Header extends Component {
     render() {
         return (
             <div>
-                Header
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a href="#!">Sua Conta</a></li>
+                    <li><a href="#!">Configurações</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Sair</a></li>
+                </ul>
+            <nav>
+                <div>
+                    <a href="/" class="brand-logo">PharmaGO</a>  
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="/drugs">Drugs</a></li>
+                        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">+</a></li>
+                    </ul>
+                </div>
+            </nav>
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 
